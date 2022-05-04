1f5d92b15d401f0432cba7c29f88c7a19834d6a9
CCLCK <84182037+CCLCK@users.noreply.github.com>
2022-05-04 23:36:05 +0800

    红黑树的插入删除模拟实现

--- RBTree/RBTree.h
+++ RBTree/RBTree.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <time.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <vector>
 using namespace std;
 
@@ -324,6 +325,22 @@ public:
 		{
 			return false;
 		}
+		if (cur == _root&&cur->_right==nullptr&&cur->_left!=nullptr)//直接删除根 且右子树为空
+		{
+			_root = cur->_left;
+			_root->_col = BLACK;
+			delete cur;
+			cur = nullptr;
+			return true;
+		}
+		if (cur == _root && cur->_right != nullptr && cur->_left == nullptr)//直接删除根 且右子树为空
+		{
+			_root = cur->_right;
+			_root->_col = BLACK;
+			delete cur;
+			cur = nullptr;
+			return true;
+		}
 
 		Node* realDel = nullptr;
 		Node* parent = cur->_parent;
@@ -356,10 +373,13 @@ public:
 				if (parent->_left == cur)
 				{
 					parent->_left = cur->_left;
+					cur->_left->_parent = parent;
 				}
 				else
 				{
 					parent->_right = cur->_left;
+					cur->_left->_parent = parent;
+
 				}
 				cur->_left->_col = BLACK;
 				delete cur;
@@ -378,12 +398,15 @@ public:
 				if (parent->_left == cur)
 				{
 					parent->_left = cur->_right;
+					cur->_right->_parent = parent;
 				}
 				else
 				{
 					parent->_right = cur->_right;
+					cur->_right->_parent = parent;
 				}
 				cur->_right->_col = BLACK;
+				
 				delete cur;
 				cur = nullptr;
 			}
@@ -396,7 +419,7 @@ public:
 		{
 			Node* minRight = cur->_right;
 			Node* minRightParent=cur ;
-			while (minRight->_left)//要不要加minRight？
+			while (minRight->_left)
 			{
 				minRightParent = minRight;
 				minRight = minRight->_left;
@@ -416,15 +439,17 @@ public:
 				delete minRight;
 				minRight = nullptr;
 			}
-			else if (minRight->_col == BLACK && minRight->_right != nullptr)//存在右子树不是叶子
+			else if (minRight->_col == BLACK && minRight->_right != nullptr)//存在右子树且不是叶子
 			{
 				if (minRightParent->_left == minRight)
 				{
 					minRightParent->_left = minRight->_right;
+					minRight->_right->_parent = minRightParent;
 				}
 				else
 				{
 					minRightParent->_right = minRight->_right;
+					minRight->_right->_parent = minRightParent;
 				}
 				minRight->_right->_col = BLACK;
 				delete minRight;
@@ -434,20 +459,183 @@ public:
 			{
 				realDel = minRight;//记录下来待处理
 			}
-			else
+			else//我认为这种情况不会有
 			{
 				cout << "存在了不符合要求的情况" << endl;
 			}
-			
+		}
+		if (realDel == nullptr)
+		{
+			return true;
+		}
+		//删除黑色叶子
+		/*
+			1.黑色叶子为根时 
+			2.兄弟侄子全黑  --> 看父亲
+			3.兄弟为黑 侄子不全黑-->看直线/折线
+			4.兄弟为红 旋转成情况2
+		*/
+		if (realDel == _root)
+		{
+			cout << "已删除根" << endl;
+			_root = nullptr;
+			return true;
+		}
+		//不是根节点 黑色叶子肯定有父亲
+		Node* del = realDel;//记录待删除的结点  
+		while (realDel != _root && realDel->_col==BLACK)
+		{
+			Node* DelParent = realDel->_parent;
+			Node* sibling;
+			if (DelParent->_left == realDel)
+			{
+				sibling = DelParent->_right;
+			}
+			else
+			{
+				sibling = DelParent->_left;
+			}
+			if (sibling == nullptr)
+			{
+				sibling= new Node(make_pair(-999, 0));
+				sibling->_col = BLACK;
+			}
+			Node* nephew_left = sibling->_left;
+			Node* nephew_right = sibling->_right;
+			if (nephew_left == nullptr)
+			{
+				nephew_left = new Node(make_pair(-999,0));
+				nephew_left->_col = BLACK;
+			}
+			if (nephew_right == nullptr)
+			{
+				nephew_right= new Node(make_pair(-999,0));
+				nephew_right->_col = BLACK;
+			}
+			if (sibling->_col == BLACK && nephew_right->_col == BLACK && nephew_left->_col == BLACK)
+			{
+				//兄弟侄子全黑
+				if (DelParent->_col == RED)
+				{
+					//父亲为红
+					DelParent->_col = BLACK;
+					sibling->_col = RED;
+					break;
+				}
+				else
+				{
+					//父亲为黑
+					sibling->_col = RED;
+					realDel = realDel->_parent;
+				}
+
+			}
+			else if (sibling->_col == BLACK && (!(nephew_right->_col == BLACK && nephew_left->_col == BLACK)))
+			{
+				//兄弟为黑 侄子不全黑
+
+				if (DelParent->_right == sibling && nephew_right->_col == RED)
+				{
+					//直线
+					sibling->_col = DelParent->_col;
+					DelParent->_col = BLACK;
+					RotateL(DelParent);
+					nephew_right->_col = BLACK;
+					
+				}
+				else if (DelParent->_left == sibling && nephew_left->_col == RED)
+				{
+					//直线
+					sibling->_col = DelParent->_col;
+					DelParent->_col = BLACK;
+					RotateR(DelParent);
+					nephew_left->_col = BLACK;
+				}
+				else if (DelParent->_left==sibling&&nephew_right->_col==RED)
+				{
+					//折线
+					RotateL(sibling);
+					nephew_right->_col = BLACK;
+					sibling->_col = RED;
+					nephew_left = sibling;
+					sibling = nephew_right;
+					RotateR(DelParent);
+					sibling->_col = DelParent->_col;
+					DelParent->_col = BLACK;
+					nephew_left->_col = BLACK;
+				}
+				else if (DelParent->_right==sibling&&nephew_left->_col==RED)
+				{
+					//折线
+					RotateR(sibling);
+					nephew_left->_col = BLACK;
+					sibling->_col = RED;
+					nephew_right = sibling;
+					sibling = nephew_left;
+					RotateL(DelParent);
+					sibling->_col = DelParent->_col;
+					DelParent->_col = BLACK;
+					nephew_right->_col = BLACK;
+				}
+				else
+				{
+					assert(false);
+				}
+				break;
+			}
+			else if (sibling->_col == RED)
+			{
+				//兄弟为红
+				if (DelParent->_left == sibling && sibling->_col == RED)
+				{
+					RotateR(DelParent);
+					sibling->_col = BLACK;
+					DelParent->_col = RED;
+				}
+				else if (DelParent->_right == sibling && sibling->_col == RED)
+				{
+					RotateL(DelParent);
+					sibling->_col = BLACK;
+					DelParent->_col = RED;
+				}
+			}
+			else
+			{
+				assert(false);
+			}
+		}
+		Node* delParent = del->_parent;
+		
+		if (delParent->_left == del)
+		{
+			delParent->_left = del->_right;
+			if (del->_right)
+			{
+				del->_right->_parent = delParent;
+			}
+			delete del;
+			del = nullptr;
+		}
+		else
+		{
+			delParent->_right = del->_right;
+			if (del->_right)
+			{
+				del->_right->_parent = delParent;
+			}
+
+			delete del;
+			del = nullptr;
 		}
 		return true;
 	}
+	
 private:
 	Node* _root;
 };
 
 
-void test1()
+void test1()//插入测试
 {
 	RBTree<int,int> rb;
 	//rb.Insert(make_pair(4, 1));
@@ -475,7 +663,7 @@ void test1()
 	cout << rb.IsBalance() << endl;
 }
 
-void test2()
+void test2()//插入随机用例测试
 {
 	RBTree<int, int> rb;
 	const int n = 1000000;
@@ -495,7 +683,7 @@ void test2()
 }
 
 
-void test3()
+void test3()//基本删除测试
 {
 	RBTree<int, int> rb;
 	rb.Insert(make_pair(27, 1));
@@ -509,13 +697,153 @@ void test3()
 	rb.Insert(make_pair(73, 1));
 	rb.Insert(make_pair(19, 1));
 	
-	//rb.Erase(make_pair(19, 1));
-	//rb.Erase(make_pair(27, 1));
-	rb.Erase(make_pair(66, 1));
+	rb.Erase(make_pair(19, 1));
+	rb.Erase(make_pair(27, 1));
+	rb.Erase(make_pair(4, 1));
+	rb.Erase(make_pair(59, 1));
+	rb.Erase(make_pair(82, 1));
+
 	cout << rb.IsBalance() << endl;
 
 }
 
 
 
+void test4()//随机删除测试
+{
+	RBTree<int, int> rb;
+	const int n = 10000;
+	vector<int>v;
+	v.reserve(n);
+	srand(time(0));
+	for (int i = 0; i < n; i++)
+	{
+		v.push_back(rand());
+	}
+	for (auto e : v)
+	{
+		//cout << e << " " << endl;
+		rb.Insert(make_pair(e, e));
+	}
+	for (auto e : v)
+	{
+		
+		rb.Erase(make_pair(e, e));
+		if (rb.IsBalance() == 0)
+		{
+			cout << "存在删除不成功" << endl;
+		}
+	}
+	cout << "删除成功" << endl;
+	
+}
 
+void test5()//随机用例测试
+{
+	RBTree<int, int> rb;
+	rb.Insert(make_pair(24, 1));
+	rb.Insert(make_pair(70, 1));
+	rb.Insert(make_pair(42, 1));
+	rb.Insert(make_pair(50, 1));
+	rb.Insert(make_pair(89, 1));
+	rb.Insert(make_pair(52, 1));
+	rb.Insert(make_pair(48, 1));
+	rb.Insert(make_pair(5, 1));
+	rb.Insert(make_pair(24, 1));
+	rb.Insert(make_pair(3, 1));
+
+	rb.Erase(make_pair(24, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(70, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(42, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(50, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(89, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(52, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(48, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(5, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(24, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(3, 1));
+	cout << rb.IsBalance() << endl;
+}
+
+void test6()//随机用例测试
+{
+	RBTree<int, int> rb;
+	rb.Insert(make_pair(12, 1));
+	rb.Insert(make_pair(88, 1));
+	rb.Insert(make_pair(11, 1));
+	rb.Insert(make_pair(29, 1));
+	rb.Insert(make_pair(41, 1));
+	rb.Insert(make_pair(73, 1));
+	rb.Insert(make_pair(96, 1));
+	rb.Insert(make_pair(49, 1));
+	rb.Insert(make_pair(26, 1));
+	rb.Insert(make_pair(18, 1));
+
+	rb.Erase(make_pair(12, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(88, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(11, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(29, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(41, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(73, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(96, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(49, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(26, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(18, 1));
+	cout << rb.IsBalance() << endl;
+}
+
+
+void test7()//随机用例测试
+{
+	RBTree<int, int> rb;
+	rb.Insert(make_pair(34, 1));
+	rb.Insert(make_pair(81, 1));
+	rb.Insert(make_pair(91, 1));
+	rb.Insert(make_pair(52, 1));
+	rb.Insert(make_pair(78, 1));
+	rb.Insert(make_pair(20, 1));
+	rb.Insert(make_pair(61, 1));
+	rb.Insert(make_pair(1, 1));
+	rb.Insert(make_pair(10, 1));
+	rb.Insert(make_pair(12, 1));
+
+	rb.Erase(make_pair(34, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(81, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(91, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(52, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(78, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(20, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(61, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(1, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(10, 1));
+	cout << rb.IsBalance() << endl;
+	rb.Erase(make_pair(12, 1));
+	cout << rb.IsBalance() << endl;
+	
+}
