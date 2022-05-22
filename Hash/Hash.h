bfa0f7f68485fb8a8937651dae4d0d877bfc6c93
CCLCK <84182037+CCLCK@users.noreply.github.com>
2022-05-22 22:11:01 +0800

    模拟实现哈希

--- Hash/Hash.h
+++ Hash/Hash.h
@@ -184,9 +184,196 @@ namespace close_hash
 
 }
 
-namespace open_hash
+//namespace open_hash
+namespace HashBucket
 {
+	template<class K,class V>
+	struct HashNode
+	{
+		pair<K, V> _kv;
+		HashNode* _next;
+		HashNode(const pair<K, V>& kv)
+			:_kv(kv)
+			, _next(nullptr)
+		{}
+	};
+	template<class K,class V,class Hash=HashFunc<K>>
+	class HashTable
+	{
+		typedef HashNode<K,V> Node;
+	public:
+
+		Node* Find(const K& key)//Find函数返回值一般都是指针，通过指针访问这个自定义类型的成员
+		{
+			Hash hash;
+			if (_tables.size() == 0)//表的大小为0，防止取余0
+			{
+				return nullptr;
+			}
+			size_t index = hash(key) % _tables.size();//找到桶号
+			Node* cur = _tables[index];
+			while (cur)
+			{
+				if (cur->_kv.first == key)
+				{
+					return cur;
+				}
+				else
+				{
+					cur = cur->_next;
+				}
+			}
+			return nullptr;
+		}
 
 
+		size_t GetNextPrime(size_t prime)
+		{
+			const int PRIMECOUNT = 28;
+			static const size_t primeList[PRIMECOUNT] =
+			{
+				53ul, 97ul, 193ul, 389ul, 769ul,
+				1543ul, 3079ul, 6151ul, 12289ul, 24593ul,
+				49157ul, 98317ul, 196613ul, 393241ul, 786433ul,
+				1572869ul, 3145739ul, 6291469ul, 12582917ul, 25165843ul,
+				50331653ul, 100663319ul, 201326611ul, 402653189ul, 805306457ul,
+				1610612741ul, 3221225473ul, 4294967291ul
+			};
+			//ul表示unsigned long
+			size_t i = 0;
+			for (; i < PRIMECOUNT; ++i)
+			{
+				if (primeList[i] > prime)
+					return primeList[i];
+			}
+
+			return primeList[i];
+		}
+
+		bool Insert(const pair<K, V>& kv)
+		{
+			if (Find(kv.first))//有相同的key直接返回false
+			{
+				return false;
+			}
+			//if(_n==0||_n==_tables.size())
+			Hash hash;
+			if (_n == _tables.size())//最开始_n为0，而_tables.size()也为0所以可以简化为一行代码
+			{
+				//增容
+				//size_t newSize = _tables.size() == 0 ? 10 : _tables.size() * 2;
+				size_t newSize = GetNextPrime(_tables.size());
+				vector<Node*>newTables;
+				newTables.resize(newSize, nullptr);
+				for (int i = 0; i < _tables.size(); i++)
+				{
+					Node* cur = _tables[i];
+					while (cur)
+					{
+						Node* next = cur->_next;//记录下一个位置
+						size_t index = hash(cur->_kv.first) % newTables.size();
+						cur->_next = newTables[index];//cur当头
+						newTables[index] = cur;//更新vector里的头
+						cur = next;
+					}
+				}
+				_tables.swap(newTables);//把新表的数据放入旧表中
+			}
+
+			size_t index = hash(kv.first) % _tables.size();//算出桶号
+			//头插
+			Node* newNode = new Node(kv);
+			newNode->_next = _tables[index];
+			_tables[index]=newNode;
+			++_n;//别忘记更新有效数据的个数
+			return true;
+		}
+		bool Erase(const K& key)
+		{ 
+			//if (!Find(key))//找不到这个元素 
+			// 这么写也可以，但是后面删除的过程中会顺带遍历整个桶
+			//{
+			//	return false;
+			//}
+			if (_tables.size() == 0)//哈希表为空
+			{
+				return false;
+			}
+			Hash hash;
+			size_t index = hash(key) % _tables.size();
+			Node* cur = _tables[index];
+			Node* prev = nullptr;//记录前一个位置
+			while (cur)
+			{
+				if (cur->_kv.first == key)//找到这个元素了
+				{
+					if(cur==_tables[index])//元素是头结点
+					{
+						_tables[index] = cur->_next;
+						
+					}
+					else//不是头结点
+					{
+						prev->_next = cur->_next;
+					}
+					delete cur;
+					cur = nullptr;
+					_n--;
+					return true;
+				}
+				else
+				{
+					prev = cur;
+					cur = cur->_next;
+				}
+			}
+			return false;
+		}
+		~HashTable()//哈希桶采用的链表结构 需要释放每个链表
+		{
+			for (int i=0;i<_tables.size();i++)
+			{
+				Node* cur = _tables[i];
+				if (cur == nullptr)
+				{
+					continue;
+				}
+				else
+				{
+					cur = cur->_next;
+				}
+				while (cur)
+				{
+					Node* next = cur->_next;
+					delete cur;
+					cur = next;
+				}
+				_tables[i] = nullptr;
+			}
+			_n = 0;
+		}
+		HashTable() {};
+
+	private:
+		vector<Node*>_tables;//存的是链表首元素的指针
+		size_t _n=0;//有效数据
+	};
+	void TestHashTable1()
+	{
+		HashTable<int, int> ht;
+		int a[] = { 4, 44, 14, 5, 2, 22, 12, 5, 8, 10, 15 };
+		for (auto e : a)
+		{
+			ht.Insert(make_pair(e, e));
+		}
+
+		ht.Insert(make_pair(11, 11));
+
+		ht.Erase(22);
+		ht.Erase(2);
+		ht.Erase(100);
+		HashTable<int, int> ht2(ht);
+
+	}
 }
 
